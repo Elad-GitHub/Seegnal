1040671183eefb4433cf1bf5cccaa447c1c8316d
Elad-GitHub <eli.cjob@gmail.com>
2021-12-27 14:18:58 +0200

    refactoring2 commit

--- Seegnal/Seegnal/Controllers/DrugController.cs
+++ Seegnal/Seegnal/Controllers/DrugController.cs
@@ -19,6 +19,8 @@ namespace Seegnal.Controllers
 
             List<IngredientData> mostProminentIngredients = GetMostProminentIngredients(drugsData);
 
+            CalculateRelativePrecentage(mostProminentIngredients);
+
             return mostProminentIngredients;
         }
 
